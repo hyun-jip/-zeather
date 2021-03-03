2cf877fb5b0209cb23d327ff1a2377782487db0e
hyun-jip <frank910@naver.com>
2021-03-03 22:38:16 +0900

    21-03-03

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://unsplash.com/photos/Vv45XEMJWZk",
+      require("./assets/splash.png"),
+    ]);
+
+    return Promise.all([...images, ...fonts]);
+  };
+
+  const onFinish = async () => setIsReady(true);
+  return isReady ? (
+    <Text>I'm Done</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={console.error}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
