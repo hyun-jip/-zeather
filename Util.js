ed84675fc07e58f9e2699e6f13d8b39b4c073dc4
hyun-jip <frank910@naver.com>
2021-03-31 00:19:25 +0900

    21-03-30

--- Util.js
+++ Util.js
@@ -18,8 +18,17 @@ export const formatSunAMPM = (time) => {
 };
 
 export const formatHour = (time) => {
-
   const unix_hour = moment.unix(time + 32400).format("H");
 
   return unix_hour;
+};
+
+export const formatFutureDate = (time) => {
+  const unix_month = moment.unix(time + 32400).format("M");
+  const unix_day = moment.unix(time + 32400).format("D");
+  const unix_weekday = moment.unix(time + 32400).day();
+  const weekdayArr = ["일", "월", "화", "수", "목", "금", "토"];
+
+  const unix_sumFutureDate = `${unix_month}월 ${unix_day}일 ${weekdayArr[unix_weekday]}요일`;
+  return unix_sumFutureDate;
 };
